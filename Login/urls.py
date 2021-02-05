87493c772d9f629454c13cb0c0b5034058bb3abf
John McCutchen <john.mccutchen@progleasing.com>
2021-02-05 07:22:21 -0500

    removed registration

--- Login/urls.py
+++ Login/urls.py
@@ -7,8 +7,7 @@ from . import views
 app_name = "Login"
 
 urlpatterns = [
-    # to use below add from .views import UserRegisterView
-    # path('register/', UserRegisterView.as_view(), name='register'),
+  
     path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
     path('password/', auth_views.PasswordChangeView.as_view()),
 ]
--- Login/views.py
+++ Login/views.py
@@ -5,13 +5,6 @@ from django.urls import reverse_lazy
 from .forms import EditProfileForm
 from django.contrib import messages
 
-# to use below do from .forms import SignUpForm and add from django.contrib.auth.forms import UserCreationForm
-# class UserRegisterView(generic.CreateView):
-#     form_class = SignUpForm
-#     template_name = 'registration/register.html'
-#     success_url = reverse_lazy('login')
-
-
 class UserEditView(generic.UpdateView):
     form_class = EditProfileForm
     template_name = 'registration/edit_profile.html'
